cb367dd836f7fe7edab32bfc0682505993bad608
Fan <xiaofanms@outlook.com>
2020-05-09 11:32:14 +0800

    自动生成type表，并进行微调。

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import { getFieldsByYaml } from './utils';
 import { generateDDLByFields } from './sqlite/index';
 
 const typeIdsYamlPath = 'sde/fsd/typeIDs.yaml';
+const marketGroupsYamlPath = 'sde/fsd/marketGroups.yaml';
 const blueprintsYamlPath = 'sde/fsd/blueprints.yaml';
 
-const fields = getFieldsByYaml(typeIdsYamlPath);
-const sql = generateDDLByFields('type', 'id', fields);
+let fields = getFieldsByYaml(typeIdsYamlPath);
+let sql = generateDDLByFields('type', 'id', fields);
+console.log(sql);
+
+fields = getFieldsByYaml(marketGroupsYamlPath);
+sql = generateDDLByFields('market_group', 'id', fields);
 console.log(sql);
--- src/sqlite/init.sql
+++ src/sqlite/init.sql
@@ -1,22 +1,36 @@
 CREATE TABLE "type" (
-	"id"	INTEGER,
-	"basePrice"	REAL,
-	"graphicID"	INTEGER,
-	"marketGroupID"	INTEGER,
-	"portionSize"	INTEGER,
-	"published"	INTEGER,
-	"sofFactionName"	TEXT,
-	"volume"	REAL,
-	PRIMARY KEY("id")
+  "id"                      INTEGER,
+  "groupID"                 INTEGER,
+  "mass"                    REAL,
+  "portionSize"             INTEGER,
+  "published"               BOOLEAN,
+  "volume"                  REAL,
+  "radius"                  REAL,
+  "graphicID"               INTEGER,
+  "soundID"                 INTEGER,
+  "iconID"                  INTEGER,
+  "raceID"                  INTEGER,
+  "sofFactionName"          TEXT,
+  "basePrice"               REAL,
+  "marketGroupID"           REAL,
+  "capacity"                REAL,
+  "metaGroupID"             INTEGER,
+  "variationParentTypeID"   INTEGER,
+  "factionID"               INTEGER,
+  "sofMaterialSetID"        INTEGER,
+  PRIMARY KEY("id")
 );
 
 CREATE TABLE "type_i18n" (
 	"id"	INTEGER,
-	"key"	TEXT NOT NULL,
-	"language"	TEXT NOT NULL,
+	"key"	TEXT NOT NULL, -- name, description
+	"language"	TEXT NOT NULL, -- zh
 	"value"	INTEGER NOT NULL,
 	PRIMARY KEY("id")
 );
+
+-- TODO type_masteries
+-- TODO type_traits
 
 CREATE TABLE "market_group" (
 	"id"	INTEGER,
