1b69ebd06c93e19bfa455ede25b560fdd807679d
Fan <xiaofanms@outlook.com>
2020-05-25 13:50:25 +0800

    解决活动数据丢失的问题。

--- src/platform/sqlite/blueprint_init.sql
+++ src/platform/sqlite/blueprint_init.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS "blueprint_activity" (
   "id"    INTEGER,
   "type"  INTEGER, -- 0 拷贝 1 发明 2 制造 3 材料研究 4 时间研究 5 反应
   "time"  INTEGER,
-  PRIMARY KEY("id")
+  PRIMARY KEY("id", "type")
 );
 CREATE TABLE IF NOT EXISTS "blueprint_material" (
   "id"           INTEGER,
