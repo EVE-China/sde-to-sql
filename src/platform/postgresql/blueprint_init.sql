e9b535d7ae985c0ae17787bc2153a73e187f2b56
Fan <xiaofanms@outlook.com>
2020-06-13 13:37:31 +0800

    完成PostgreSQL的蓝图部分的sql。

---
+++ src/platform/postgresql/blueprint_init.sql
@@ -0,0 +1,34 @@
+-- 蓝图
+CREATE TABLE eve."blueprint" (
+  "id"                  integer,
+  "maxProductionLimit"  integer,
+  PRIMARY KEY("id")
+);
+CREATE TABLE eve."blueprint_activity" (
+  "id"    integer,
+  "type"  integer, -- 0 拷贝 1 发明 2 制造 3 材料研究 4 时间研究 5 反应
+  "time"  integer,
+  PRIMARY KEY("id", "type")
+);
+CREATE TABLE eve."blueprint_material" (
+  "id"           integer,
+  "activityType" integer,
+  "typeID"       integer,
+  "quantity"     integer,
+  PRIMARY KEY("id", "activityType", "typeID")
+);
+CREATE TABLE eve."blueprint_skill" (
+  "id"           integer,
+  "activityType" integer,
+  "typeID"       integer,
+  "level"        integer,
+  PRIMARY KEY("id", "activityType", "typeID")
+);
+CREATE TABLE eve."blueprint_product" (
+  "id"           integer,
+  "activityType" integer,
+  "probability"  real,
+  "typeID"       integer,
+  "quantity"     integer,
+  PRIMARY KEY("id", "activityType", "typeID")
+);
